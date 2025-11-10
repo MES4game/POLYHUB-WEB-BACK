eadfe3bd33aff77b2830f3f52164ffa99ae69bae
MES4game <mes4.game@gmail.com>
2025-11-10 01:29:05 +0100

    refactor(scrum-9-data_api): little refactor

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,5 +4,5 @@ import { RequestError } from "@/models/common.model";
 
 export function errorHandler(err: unknown, _req: Request, res: Response, _next: NextFunction) {
     if (err instanceof RequestError) res.status(err.code).json(err.message);
-    else                             res.status(500).json(unknowErrToString(err) || "Internal Server Error");
+    else res.status(500).json(unknowErrToString(err) || "Internal Server Error");
 }
