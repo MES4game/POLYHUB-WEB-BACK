bae2897fc1e8493ad9a7a9e08e99ee4de8aa6e24
MES4game <mes4.game@gmail.com>
2025-11-12 17:36:35 +0100

    fix(scrum-9-data_api): cors fix

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,11 @@ import cors from "cors";
 import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import rateLimit from "express-rate-limit";
-import { ENV } from "@/config/env.config";
 import { errorHandler } from "@/middlewares/error.middleware";
 import { RegisterRoutes } from "@/routes";
 import * as SWAGGER_SPEC from "@/swagger.json";
 
 const app = express();
-
-app.use(express.json());
 
 // Extern Middlewares
 app.use(helmet());
@@ -21,16 +18,15 @@ app.use(express.json({ limit: "10kb" }));
 app.use(morgan("combined"));
 
 app.use(cors({
-    origin     : `https://${ENV.host}`,
     credentials: true,
 }));
 
 app.use(rateLimit({
-    windowMs: 60 * 1000,
-    max: 300,
-    message: 'You have exceeded the 100 requests limit in 1 minute. Please try again later.',
+    windowMs       : 60 * 1000,
+    max            : 300,
+    message        : "You have exceeded the 300 requests limit in 1 minute. Please try again later.",
     standardHeaders: true,
-    legacyHeaders: false,
+    legacyHeaders  : false,
 }));
 
 /* eslint-disable */
